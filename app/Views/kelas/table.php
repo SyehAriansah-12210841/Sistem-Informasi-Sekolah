b81a7561b7a4f9b8940bd1b88302442d78df3ad9
FeniAuliaSari-12210590 <113705419+FeniAuliaSari-12210590@users.noreply.github.com>
2022-10-25 00:43:50 +0700

    menambahkan table

---
+++ app/Views/kelas/table.php
@@ -0,0 +1,52 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/gh/agoenxz2186/submitAjax@develop/submit_ajax.js"></script>
+    <link href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css" rel="stylesheet">
+    <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+<table id='table-kelas' class="datatable table table-bordered border-primary">
+    <thead>
+        <tr class='table-primary'>
+            <th>No</th>
+            <th>Tingkat</th>
+            <th>Kelas</th>
+            <th>Pegawai Id</th>
+            <th>Tahun Ajaran Id</th>
+            <th>Aksi</th>
+            
+
+        </tr>
+    </thead>
+</table>
+<script>
+    $(document).ready(function(){
+        $('table#table-kelas').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax:{
+                url: "<?=base_url('kelas/all')  ?>",
+                method: 'GET'
+            },
+            columns:[
+                {data: 'id', sortable:false, searchable:false,
+                render: (data,type,row,meta)=>{
+                    return meta.settings._iDisplayStart + meta.row + 1;
+                }
+            },
+                // {data: 'id'},
+                {data: 'tingkat'},
+                {data: 'kelas'},
+                {data: 'pegawai_id'},
+                {data: 'tahun_ajaran_id'},               
+                {data: 'id',
+                    render: (data, type, meta, row)=>{
+                        var btnEdit = `<button class='btn-edit' data-id='${data}'> Edit </button>`;
+                        var btnHapus = `<button class='btn-hapus' data-id='${data}'> Hapus </button>`;
+                        return btnEdit + btnHapus;
+                    }
+                }
+            ]
+        });
+    });
+</script>
