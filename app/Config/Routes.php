30f385f320210eb251e81a173a2b78bc9f9daec1
alfinaagustini12210692 <113718304+alfinaagustini12210692@users.noreply.github.com>
2022-10-25 00:46:55 +0700

    menambahkan route agar bisa di akses

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -2,6 +2,8 @@
 
 namespace Config;
 
+use CodeIgniter\Router\RouteCollection;
+
 // Create a new instance of our RouteCollection class.
 $routes = Services::routes();
 
@@ -37,6 +39,131 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 
+$routes->group('login', function(RouteCollection $routes){
+    $routes->get('lupa', 'PegawaiController::viewLupaPassword');
+    $routes->get('/', 'PegawaiController::viewLogin');
+    $routes->post('/', 'PegawaiController::login');
+    $routes->delete('/', 'PegawaiController::logout');
+    $routes->patch('/', 'PegawaiController::lupaPassword');
+
+});
+
+$routes->group('Pegawai', function(RouteCollection $routes){
+    $routes->get('/', 'PegawaiController::index');
+    $routes->post('/', 'PegawaiController::store');
+    $routes->patch('/', 'PegawaiController::update');
+    $routes->delete('/', 'PegawaiController::delete');
+    $routes->get('(:num)', 'PegawaiController::show/$1');
+    $routes->get('all', 'PegawaiController::all');
+});
+
+$routes->group('bagian', function(RouteCollection $routes){
+    $routes->get('/', 'BagianController::index');
+    $routes->post('/', 'BagianController::store');
+    $routes->patch('/', 'BagianController::update');
+    $routes->delete('/', 'BagianController::delete');
+    $routes->get('(:num)', 'BagianController::show/$1');
+    $routes->get('all', 'BagianController::all');
+});
+
+$routes->group('tahun', function(RouteCollection $routes){
+    $routes->get('/', 'TahunAjaranController::index');
+    $routes->post('/', 'TahunAjaranController::store');
+    $routes->patch('/', 'TahunAjaranController::update');
+    $routes->delete('/', 'TahunAjaranController::delete');
+    $routes->get('(:num)', 'TahunAjaranController::show/$1');
+    $routes->get('all', 'TahunAjaranController::all');
+});
+
+$routes->group('pendidikanguru', function(RouteCollection $routes){
+    $routes->get('/', 'PendidikanGuruController::index');
+    $routes->post('/', 'PendidikanGuruController::store');
+    $routes->patch('/', 'PendidikanGuruController::update');
+    $routes->delete('/', 'PendidikanGuruController::delete');
+    $routes->get('(:num)', 'PendidikanGuruController::show/$1');
+    $routes->get('all', 'PendidikanGuruController::all');
+});
+
+$routes->group('kelas', function(RouteCollection $routes){
+    $routes->get('/', 'KelasController::index');
+    $routes->post('/', 'KelasController::store');
+    $routes->patch('/', 'KelasController::update');
+    $routes->delete('/', 'KelasController::delete');
+    $routes->get('(:num)', 'KelasController::show/$1');
+    $routes->get('all', 'KelasController::all');
+});
+
+$routes->group('mapel', function(RouteCollection $routes){
+    $routes->get('/', 'MapelController::index');
+    $routes->post('/', 'MapelController::store');
+    $routes->patch('/', 'MapelController::update');
+    $routes->delete('/', 'MapelController::delete');
+    $routes->get('(:num)', 'MapelController::show/$1');
+    $routes->get('all', 'MapelController::all');
+});
+
+$routes->group('jadwal', function(RouteCollection $routes){
+    $routes->get('/', 'JadwalController::index');
+    $routes->post('/', 'JadwalController::store');
+    $routes->patch('/', 'JadwalController::update');
+    $routes->delete('/', 'JadwalController::delete');
+    $routes->get('(:num)', 'JadwalController::show/$1');
+    $routes->get('all', 'JadwalController::all');
+});
+
+$routes->group('kehadiranguru', function(RouteCollection $routes){
+    $routes->get('/', 'KehadiranGuruController::index');
+    $routes->post('/', 'KehadiranGuruController::store');
+    $routes->patch('/', 'KehadiranGuruController::update');
+    $routes->delete('/', 'KehadiranGuruController::delete');
+    $routes->get('(:num)', 'KehadiranGuruController::show/$1');
+    $routes->get('all', 'KehadiranGuruController::all');
+});
+
+$routes->group('siswa', function(RouteCollection $routes){
+    $routes->get('/', 'SiswaController::index');
+    $routes->post('/', 'SiswaController::store');
+    $routes->patch('/', 'SiswaController::update');
+    $routes->delete('/', 'SiswaController::delete');
+    $routes->get('(:num)', 'SiswaController::show/$1');
+    $routes->get('all', 'SiswaController::all');
+});
+
+$routes->group('kelassiswa', function(RouteCollection $routes){
+    $routes->get('/', 'KelasSiswaController::index');
+    $routes->post('/', 'KelasSiswaController::store');
+    $routes->patch('/', 'KelasSiswaController::update');
+    $routes->delete('/', 'KelasSiswaController::delete');
+    $routes->get('(:num)', 'KelasSiswaController::show/$1');
+    $routes->get('all', 'KelasSiswaController::all');
+});
+
+$routes->group('kehadiransiswa', function(RouteCollection $routes){
+    $routes->get('/', 'KehadiranSiswaController::index');
+    $routes->post('/', 'KehadiranSiswaController::store');
+    $routes->patch('/', 'KehadiranSiswaController::update');
+    $routes->delete('/', 'KehadiranSiswaController::delete');
+    $routes->get('(:num)', 'KehadiranSiswaController::show/$1');
+    $routes->get('all', 'KehadiranSiswaController::all');
+});
+
+$routes->group('penilaian', function(RouteCollection $routes){
+    $routes->get('/', 'PenilaianController::index');
+    $routes->post('/', 'PenilaianController::store');
+    $routes->patch('/', 'PenilaianController::update');
+    $routes->delete('/', 'PenilaianController::delete');
+    $routes->get('(:num)', 'PenilaianController::show/$1');
+    $routes->get('all', 'PenilaianController::all');
+});
+
+$routes->group('rincianpenilaian', function(RouteCollection $routes){
+    $routes->get('/', 'RincianPenilaianController::index');
+    $routes->post('/', 'RincianPenilaianController::store');
+    $routes->patch('/', 'RincianPenilaianController::update');
+    $routes->delete('/', 'RincianPenilaianController::delete');
+    $routes->get('(:num)', 'RincianPenilaianController::show/$1');
+    $routes->get('all', 'RincianPenilaianController::all');
+});
 /*
  * --------------------------------------------------------------------
  * Additional Routing
